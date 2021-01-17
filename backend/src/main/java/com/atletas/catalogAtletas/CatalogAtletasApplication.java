7fcf113d13fafd14a79fcdd50431932bcb1b178c
unknown <leofelipe.a@gmail.com>
2021-01-17 08:43:59 -0300

    Project Created

---
+++ backend/src/main/java/com/atletas/catalogAtletas/CatalogAtletasApplication.java
@@ -0,0 +1,13 @@
+package com.atletas.catalogAtletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogAtletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogAtletasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/atletas/catalogAtletas/CatalogAtletasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atletas.catalogAtletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogAtletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
