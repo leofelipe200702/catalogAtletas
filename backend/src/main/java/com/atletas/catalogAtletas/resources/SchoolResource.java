d3dca2e5e48df4d742aeb03c3abf300330dc52d3
unknown <leofelipe.a@gmail.com>
2021-01-18 17:44:27 -0300

    finish school

--- backend/src/main/java/com/atletas/catalogAtletas/resources/SchoolResource.java
+++ backend/src/main/java/com/atletas/catalogAtletas/resources/SchoolResource.java
@@ -1,12 +1,19 @@
 package com.atletas.catalogAtletas.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.atletas.catalogAtletas.dto.SchoolDTO;
 import com.atletas.catalogAtletas.services.SchoolService;
@@ -23,7 +30,43 @@ public class SchoolResource {
 		List<SchoolDTO> list = schoolService.findAll();
 
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<SchoolDTO> findById(@PathVariable Long id) {
+		SchoolDTO dto = schoolService.findById(id);
+		return ResponseEntity.ok(dto);
 
 	}
+
+	@PostMapping
+	public ResponseEntity<SchoolDTO> save(@RequestBody SchoolDTO school) {
+
+		school = schoolService.save(school);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(school.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).body(school);
+
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<SchoolDTO> update(@PathVariable Long id, @RequestBody SchoolDTO school) {
+
+		school = schoolService.update(id, school);
+
+		return ResponseEntity.ok().body(school);
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<SchoolDTO> delete(@PathVariable Long id) {
+
+		schoolService.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
 
 }
--- backend/src/main/java/com/atletas/catalogAtletas/services/SchoolService.java
+++ backend/src/main/java/com/atletas/catalogAtletas/services/SchoolService.java
@@ -1,15 +1,22 @@
 package com.atletas.catalogAtletas.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.atletas.catalogAtletas.dto.SchoolDTO;
 import com.atletas.catalogAtletas.entities.School;
 import com.atletas.catalogAtletas.repositories.SchoolRepository;
+import com.atletas.catalogAtletas.services.exception.DataBaseException;
+import com.atletas.catalogAtletas.services.exception.ResourceNotFoundException;
 
 @Service
 public class SchoolService {
@@ -21,6 +28,50 @@ public class SchoolService {
 	public List<SchoolDTO> findAll() {
 		List<School> list = schoolRepository.findAll();
 		return list.stream().map(x -> new SchoolDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public SchoolDTO findById(Long id) {
+		Optional<School> obj = schoolRepository.findById(id);
+
+		School entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+		return new SchoolDTO(entity);
+	}
+
+	@Transactional
+	public SchoolDTO save(SchoolDTO dto) {
+
+		School entity = new School();
+		entity.setName(dto.getName());
+		entity = schoolRepository.save(entity);
+
+		return new SchoolDTO(entity);
+	}
+
+	@Transactional
+	public SchoolDTO update(Long id, SchoolDTO dto) {
+
+		try {
+			School entity = schoolRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity = schoolRepository.save(entity);
+			return new SchoolDTO(entity);
+		} catch (EntityNotFoundException ex) {
+			throw new ResourceNotFoundException("Id Not Found " + id);
+		}
+
+	}
+
+	public void delete(Long id) {
+		try {
+			schoolRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id Not Found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity Violation");
+		}
+
 	}
 
 }
