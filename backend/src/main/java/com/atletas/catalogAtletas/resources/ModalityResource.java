dd28773a3bd408289c049db4a2c683d5d78be040
unknown <leofelipe.a@gmail.com>
2021-01-18 17:31:03 -0300

    finish Modality

--- backend/src/main/java/com/atletas/catalogAtletas/resources/ModalityResource.java
+++ backend/src/main/java/com/atletas/catalogAtletas/resources/ModalityResource.java
@@ -1,13 +1,19 @@
 package com.atletas.catalogAtletas.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.atletas.catalogAtletas.dto.ModalityDTO;
 import com.atletas.catalogAtletas.services.ModalityService;
 
@@ -23,6 +29,42 @@ public class ModalityResource {
 		List<ModalityDTO> list = modalityService.findAll();
 
 		return ResponseEntity.ok(list);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ModalityDTO> findById(@PathVariable Long id) {
+		ModalityDTO dto = modalityService.findById(id);
+		return ResponseEntity.ok(dto);
+
+	}
+
+	@PostMapping
+	public ResponseEntity<ModalityDTO> save(@RequestBody ModalityDTO modality) {
+
+		modality = modalityService.save(modality);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(modality.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).body(modality);
+
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ModalityDTO> update(@PathVariable Long id, @RequestBody ModalityDTO modality) {
+
+		modality = modalityService.update(id, modality);
+
+		return ResponseEntity.ok().body(modality);
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ModalityDTO> delete(@PathVariable Long id) {
+
+		modalityService.delete(id);
+		return ResponseEntity.noContent().build();
 
 	}
 
--- backend/src/main/java/com/atletas/catalogAtletas/services/ModalityService.java
+++ backend/src/main/java/com/atletas/catalogAtletas/services/ModalityService.java
@@ -1,15 +1,22 @@
 package com.atletas.catalogAtletas.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.atletas.catalogAtletas.dto.ModalityDTO;
 import com.atletas.catalogAtletas.entities.Modality;
 import com.atletas.catalogAtletas.repositories.ModalityRepository;
+import com.atletas.catalogAtletas.services.exception.DataBaseException;
+import com.atletas.catalogAtletas.services.exception.ResourceNotFoundException;
 
 @Service
 public class ModalityService {
@@ -21,6 +28,51 @@ public class ModalityService {
 	public List<ModalityDTO> findAll() {
 		List<Modality> list = modalityRepository.findAll();
 		return list.stream().map(x -> new ModalityDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public ModalityDTO findById(Long id) {
+		Optional<Modality> obj = modalityRepository.findById(id);
+
+		Modality entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+		return new ModalityDTO(entity);
+	}
+
+	@Transactional
+	public ModalityDTO save(ModalityDTO dto) {
+
+		Modality entity = new Modality();
+		entity.setName(dto.getName());
+		entity = modalityRepository.save(entity);
+
+		return new ModalityDTO(entity);
+	}
+
+	@Transactional
+	public ModalityDTO update(Long id, ModalityDTO dto) {
+
+		try {
+			Modality entity = modalityRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity = modalityRepository.save(entity);
+			return new ModalityDTO(entity);
+		} catch (EntityNotFoundException ex) {
+			throw new ResourceNotFoundException("Id Not Found " + id);
+		}
+
+	}
+
+	public void delete(Long id) {
+		try {
+			modalityRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id Not Found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity Violation");
+		}
+		
 	}
 
 }
